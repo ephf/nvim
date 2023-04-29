1898f9fc936c08db80b24fd967623f2d3112f7f2
ephf <grantrwittmann@gmail.com>
2023-04-29 15:14:38 -0400

    script fix

--- install.sh
+++ install.sh
@@ -8,7 +8,10 @@ npm i -g yarn
 
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
-mv nvim.appimage usr/local/bin/nv
+mv nvim.appimage /usr/local/bin/nv
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 nv init.vim
 read -p "Run :PlugInstall then press [Enter] to continue"
